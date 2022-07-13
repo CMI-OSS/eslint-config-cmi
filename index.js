189bdb2e864fe6da73691544d1329d528c15b75c
nohgijin <ynda11111@naver.com>
2022-07-13 15:25:10 +0900

    feat: plugin 이름 변경

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
     "parser": "@typescript-eslint/parser",
     "plugins": [
-        "@typescript-eslint",
+        "@typescript-eslint/eslint-plugin",
         "prettier",
         "jsx-a11y",
         "import"
